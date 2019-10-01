52cf4880fd5043fbe4832ba8e1582d953e9d2316
Balon Milan <Balon.Milan@azd.cz>
2019-10-01 11:21:40 +0200

    - zkouska zprovozneni alzy

--- price_monitor/settings.py
+++ price_monitor/settings.py
@@ -7,7 +7,7 @@ SPIDER_MODULES = ['price_monitor.spiders']
 NEWSPIDER_MODULE = 'price_monitor.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -20,3 +20,4 @@ ITEM_PIPELINES = {
 AUTOTHROTTLE_ENABLED = True
 # HTTPCACHE_ENABLED = True
 DOWNLOADER_CLIENT_TLS_METHOD = 'TLS'
+COOKIES_ENABLED = False
